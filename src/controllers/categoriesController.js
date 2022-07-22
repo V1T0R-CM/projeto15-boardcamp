32faf1ea042113bc4742793fe59e7f885506d2d0
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-07-22 12:23:20 -0300

    Rota get categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,10 +1,16 @@
 import connection from "../db/database.js";
 
 export async function postCategories(req, res){
-    console.log(req.body.name)
     await connection.query(
         `INSERT INTO categories (name) 
           VALUES ('${req.body.name}')`
     );
     return res.sendStatus(201)
+}
+
+export async function getCategories(req, res){
+    console.log(req.body.name)
+    const { rows: categories } = await connection.query(`SELECT * FROM categories`);
+    
+    return res.status(200).send(categories)
 }
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { getCategories } from '../controllers/categoriesController.js';
 import { postCategories } from '../controllers/categoriesController.js';
 import { postCategoriesValidation } from '../middlewares/categoriesMiddlewares.js';
 
 const categoriesRouter = Router();
+categoriesRouter.get("/categories", getCategories);
 categoriesRouter.post("/categories", postCategoriesValidation, postCategories);
 export default categoriesRouter;
